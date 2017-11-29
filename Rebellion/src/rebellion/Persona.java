f65b8bbb1503fd0dd20d5f07cf0714f64c3c20bf
Angel Encalada <angaenca@espol.edu.ec>
2017-11-29 18:32:16 -0500

    Crear la clase Persona como abstracta para aplicar herencia a policias y agentes.

---
+++ Rebellion/src/rebellion/Persona.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rebellion;
+import java.util.ArrayList;
+/**
+ *
+ * @author Usuario
+ */
+public abstract class Persona {
+    private int radio;
+    private ArrayList<Posicion> vision;
+    
+}
