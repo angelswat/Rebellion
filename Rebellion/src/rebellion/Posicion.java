522136f585e15b0decad5ae6eb90ce5c886cecb4
Angel Encalada <angaenca@espol.edu.ec>
2017-11-29 17:37:31 -0500

    Crear la clase Posicion y sus atributos

---
+++ Rebellion/src/rebellion/Posicion.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rebellion;
+
+/**
+ *
+ * @author Angel Encalada
+ */
+public class Posicion {
+    private int posx;
+    private int posy;
+    
+    public Posicion(int posx, int posy){
+        this.posx=posx;
+        this.posy=posy;
+    }
+
+    public int getPosx() {
+        return posx;
+    }
+
+    public void setPosx(int posx) {
+        this.posx = posx;
+    }
+
+    public int getPosy() {
+        return posy;
+    }
+
+    public void setPosy(int posy) {
+        this.posy = posy;
+    }
+    
+    
+}
