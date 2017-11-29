204ae64bc69f0fd7b7e88bba82c13728d422a877
Angel Encalada <angaenca@espol.edu.ec>
2017-11-29 17:23:29 -0500

    Crear la clase Policia, definir sus atributos y algunos metodos.

---
+++ Rebellion/src/rebellion/Policia.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rebellion;
+import java.util.ArrayList;
+/**
+ *
+ * @author Angel Encalada
+ */
+public class Policia {
+    private ArrayList<Posicion> vision;
+    private int numFil;
+    private int numCol;
+    private Posicion posPolicia;
+    private int rango;
+    
+    public Policia(int numFil, int numCol, int rango){
+        this.numCol=numCol;
+        this.numFil=numFil;
+    }
+    
+    private void crearPosAleay(){
+        
+    }
+    
+    private void llenarRango(){
+        
+    }
+}
