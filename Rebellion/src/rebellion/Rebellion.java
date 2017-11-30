ead9bbecc8efe466b295c6c1fdc95d230c43fc37
Angel Encalada <angaenca@espol.edu.ec>
2017-11-30 08:49:13 -0500

    Modificar algunos metodos, cambiar a static, y aniadir testeos de salida.

--- Rebellion/src/rebellion/Rebellion.java
+++ Rebellion/src/rebellion/Rebellion.java
@@ -10,19 +10,94 @@ import java.util.Scanner;
  * @author Angel Encalada
  */
 public class Rebellion {
-    static double denPol;
-    static double denAg;
-    static int numFil;
-    static int numCol;
-    static int radio;
-    static int maxTurno;
-    static int movimiento;
-    static double legitimidad;
+    static double denPol=0.2;
+    static double denAg=0.6;
+    static int numFil=5;
+    static int numCol=5;
+    static int radio=2;
+    static int maxTurno=5;
+    static int movimiento=1;
+    static double legitimidad=0.8;
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         ingresarDatos();
+        salida();
+        System.out.println("Salir");
+    }
+    
+    public static void ingresarDatos(){
+        Scanner scan=new Scanner(System.in);
+        
+        System.out.println("Ingrese la densidad de Policias: ");
+        while(!scan.hasNextDouble()){
+            System.out.println("Numero invalido");
+            System.out.println("Ingrese la densidad de Policias: ");
+            scan.next();
+        }
+        Rebellion.denPol=scan.nextDouble();
+        
+        System.out.println("Ingrese la densidad de Agentes: ");
+        while(!scan.hasNextDouble()){
+            System.out.println("Numero invalido");
+            System.out.println("Ingrese la densidad de Agentes: ");
+            scan.next();
+        }
+        Rebellion.denAg=scan.nextDouble();
+       
+        System.out.println("Ingrese el numero de filas: ");
+        while(!scan.hasNextInt()){
+            System.out.println("Numero invalido");
+            System.out.println("Ingrese el numero de filas: ");
+            scan.next();
+        }
+        Rebellion.numFil=scan.nextInt();
+        
+        System.out.println("Ingrese el numero de columnas: ");
+        while(!scan.hasNextInt()){
+            System.out.println("Numero invalido");
+            System.out.println("Ingrese el numero de columnas: ");
+            scan.next();
+        }
+        Rebellion.numCol=scan.nextInt();
+        
+        System.out.println("Ingrese el numero maximo de turnos: ");
+        while(!scan.hasNextInt()){
+            System.out.println("Numero invalido");
+            System.out.println("Ingrese el numero maximo de turnos: ");
+            scan.next();
+        }
+        Rebellion.maxTurno=scan.nextInt();
+        
+        System.out.println("Ingrese el radio de vision: ");
+        while(!scan.hasNextInt()){
+            System.out.println("Numero invalido");
+            System.out.println("Ingrese el radio de vision: ");
+            scan.next();
+        }
+        Rebellion.radio=scan.nextInt();
+        
+        System.out.println("Ingrese la legitimidad del gobierno: ");
+        while(!scan.hasNextDouble()){
+            System.out.println("Numero invalido");
+            System.out.println("Ingrese la legitimidad del gobierno: ");
+            scan.next();
+        }
+        Rebellion.legitimidad=scan.nextDouble();
+        
+        System.out.println("Ingrese el estado de movimiento: ");
+        while(!scan.hasNextInt()){
+            System.out.println("Numero invalido");
+            System.out.println("Ingrese el estado de movimiento: ");
+            scan.next();
+        }
+        Rebellion.movimiento=scan.nextInt();
+         
+    }
+    
+    public static void salida(){
         int i=0;
         int j=0;
         int k=0;
@@ -30,64 +105,17 @@ public class Rebellion {
             do{
             Universo.colocarPolicias();
             i++;
-            }while(i<=Math.round(denPol*100));
+            }while(i<=Math.round(denPol*numFil*numCol));
+            System.out.println("Termino policias");
             do{
             Universo.colocarAgentes();
-            }while(j<=Math.round(denAg*100));
+            j++;
+            }while(j<=Math.round(denAg*numFil*numCol));
+            System.out.println("Termino agentes");
             Universo.imprimirUniverso();
-            Universo.generarCsv(maxTurno, numFil, numFil, numFil); //corregir valores
+            Universo.generarCsv(k);
             Universo.encerarTablero();
             k++;
         }while(k<=maxTurno);
     }
-    
-    public static void ingresarDatos(){
-        Scanner scan=new Scanner(System.in);
-        double denPol;
-        double denAg=0;
-        int numFil;
-        int numCol;
-        int maxTurno;
-        int radio;
-        double legitimidad;
-        int movimiento;
-        
-        System.out.println("Ingresar denpol:");
-        if (scan.hasNextDouble()) {
-            Rebellion.denPol= scan.nextDouble();
-        }
-        
-        System.out.println("Ingresar denag:");
-        if (scan.hasNextDouble()) {
-            Rebellion.denAg = scan.nextDouble();
-        }
-        System.out.println("Ingresar filas:");
-        if (scan.hasNextInt()) {
-            Rebellion.numFil = scan.nextInt();
-        }
-        System.out.println("Ingresar columnas:");
-        if (scan.hasNextInt()) {
-            Rebellion.numCol = scan.nextInt();
-        }
-        System.out.println("Ingresar turnos:");
-        if (scan.hasNextInt()) {
-            Rebellion.maxTurno = scan.nextInt();
-        }
-        System.out.println("Ingresar radio:");
-        if (scan.hasNextInt()) {
-            Rebellion.radio = scan.nextInt();
-        }
-        System.out.println("Ingresar legitimidad:");
-        if (scan.hasNextDouble()) {
-            Rebellion.legitimidad = scan.nextDouble();
-        }
-        System.out.println("Ingresar movimiento:");
-        if (scan.hasNextInt()) {
-            Rebellion.movimiento = scan.nextInt();
-        }    
-    }
-    
-    
-    
-    
 }
