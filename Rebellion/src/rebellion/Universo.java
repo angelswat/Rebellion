9a32dde72c4934865d91d5f1d8860de6924ec40f
Angel Encalada <angaenca@espol.edu.ec>
2017-11-29 23:10:20 -0500

    Llenar y modificar metodos de colocar policias o agentes, imprimir tablero, etc.

--- Rebellion/src/rebellion/Universo.java
+++ Rebellion/src/rebellion/Universo.java
@@ -6,21 +6,19 @@
 package rebellion;
 import java.util.Random;
 import java.util.ArrayList;
+import java.util.Collections;
 /**
  *
  * @author Usuario
  */
 public class Universo {
-    private int numFil;
-    private int numCol;
-    private int agentes;
-    private int policias;
-    private ArrayList<Posicion> tablero=new ArrayList<>();
-    
-    public Universo(){
-        numFil=10;
-        numCol=10;
-    }
+    private static int numFil;
+    private static int numCol;
+    private static int agentes;
+    private static int policias;
+    private static ArrayList<Posicion> tablero=new ArrayList<>();
+    private static ArrayList<Agente> agenttes=new ArrayList<>();
+    private static ArrayList<Policia> cops=new ArrayList<>();
     
     public Universo(int numFil, int numCol, int agentes, int policias){
         this.numFil=numFil;
@@ -29,14 +27,15 @@ public class Universo {
         this.policias=policias;
     }
 
-    public void colocarPolicias(){
+    public static void colocarPolicias(){
         int posFila;
         int posCol;
         boolean existe=false;
+        int i=0;
         Random r=new Random();
         do{
-            posFila=r.nextInt(this.numFil-1);
-            posCol=r.nextInt(this.numCol-1);
+            posFila=r.nextInt(numFil-1);
+            posCol=r.nextInt(numCol-1);
             for (Posicion par:tablero){
                 if (par.getPosx()==posFila && par.getPosy()==posCol){
                     existe=true;
@@ -47,16 +46,18 @@ public class Universo {
         while(existe==true);
         Posicion coord=new Posicion(posFila,posCol);
         tablero.add(coord);
+        Policia a=new Policia(coord);
+        cops.add(a);
     }
     
-    public void colocarAgentes(){
+    public static void colocarAgentes(){
         int posFila;
         int posCol;
         boolean existe=false;
         Random r=new Random();
         do{
-            posFila=r.nextInt(this.numFil-1);
-            posCol=r.nextInt(this.numCol-1);
+            posFila=r.nextInt(numFil-1);
+            posCol=r.nextInt(numCol-1);
             for (Posicion par:tablero){
                 if (par.getPosx()==posFila && par.getPosy()==posCol){
                     existe=true;
@@ -67,26 +68,42 @@ public class Universo {
         while(existe==true);
         Posicion coord=new Posicion(posFila,posCol);
         tablero.add(coord);
+        Agente a=new Agente(1,coord);
+        agenttes.add(a);
+
     }
-   
-    public int indentificarRebeldes(){
-        //para identificar rebeldes
+   /*
+    public int indentificarRebeldes(Agente agente){
+        for (Agente par:coordenadas){
+            //if (par instanceof Agente){
+                
+            //}
+        }
         return 1;
     }
-    
-    public void encarcelar(){
-        //para encarcelar activos segun las variables
+    */
+    public boolean encarcelar(Agente agente){
+        if(agente.getEstado()==1){
+            return true;
+        }
+        return false;
     }
     
     public void imprimirUniverso(){
-        //imprimir como va cambiando
+        //for (int i=0;i<this.numFil;i++){
+          //  for (int j=0;j<this.numCol;j++){
+                for (Posicion coord:tablero){
+                    System.out.println(coord);
+                }
+            //}
+       // }
     }
     
     public boolean identificarAgVision(){
         //testear rebeldes por rango de vision
         return true;
     }
-    
+
     public int contarEncarcelados(){
         //contabilizar presos para aniadir al csv
         return 1;
@@ -130,6 +147,6 @@ public class Universo {
     public void setPolicias(int policias) {
         this.policias = policias;
     }
-    
+
     
 }
