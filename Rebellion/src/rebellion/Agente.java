31fe56749a3acc0a7b2d73e59276526714806931
Angel Encalada <angaenca@espol.edu.ec>
2017-11-30 21:41:24 -0500

    Adicion de cabecera principal, y comentado de codigo

--- Rebellion/src/rebellion/Agente.java
+++ Rebellion/src/rebellion/Agente.java
@@ -1,18 +1,20 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package rebellion;
-import java.util.Random;
-/**
- * @author Angel Encalada
- * @param La clase Agente define las variables que tomaran los civiles.
+ * Rebellion Agent Model. Noviembre, 2017.
+ * Copyright (c) 2017 Angel Encalada.
+ * Escuela Superior Politecnica del Litoral
+ * Todos los derechos reservados.
  */
 
-//Definicion de metodos de la clase Agente
+package rebellion;
+
+/**
+ * @author Angel Encalada
+ * @param Definir los atributos y metodos exclusivos para Agentes.
+ */
+
+//Inicio de la clase.
 public class Agente extends Persona {
-    //Perjucio percibido al momento de nacer, es una valor random.
+    //Perjucio percibido al momento de nacer, es un valor random.
     private double perjuicioPercibido;
     //El agravio es un valor determinado con el perjucio percibido y la
     //legitimidad del gobierno.
@@ -27,12 +29,15 @@ public class Agente extends Persona {
     private double probDetEst;
     //Define cuan arriesgado es un civil dentro del universo.
     private double riesgoN;
+    //Objeto Agente que se guia por una coordenada dentro del universo.
     private Posicion posAgente;
+    //Constante proporcional K.
     public static final double K=2.3;
+    //Limite de agravio para todo el universo.
     public static final double LIMITE=0.1;
     //Fin de la definicion de metodos
     
-    //Inicio del constructor de la clase Agente
+    //Constructor de la clase Agente.
     public Agente(int estado, Posicion posAgente,double probabilidad,double perjuicioPercibido,double agravio,double aversion,double riesgo) {
         super();
         this.perjuicioPercibido=perjuicioPercibido=Math.random();
@@ -43,32 +48,39 @@ public class Agente extends Persona {
         this.posAgente=posAgente;
         this.probDetEst = probabilidad;
         this.riesgoN = calcularRiesgo();
-        
     }
-/*
-    public Agente(int estado,Posicion posAgente) {
-        this.estado = estado;
-        this.posAgente = posAgente;
-        verificarEstado();
-    }
-    //Fin del constructor de la clase
+    
+    /**
+    * Metodo calculador del agravio de un agente al nacer.
+    * @return Agravio del civil, en base al perjuicio y legitimidad del gobierno.
     */
-    //
     protected double calcularAgravio() {
         double agravio=perjuicioPercibido*(1-legitimidad);
         return agravio;
     }
 
+    /**
+    * Metodo calculador de la probabilidad de detencion de un civil.
+    * @return Probabilidad basada en el numero de policias y civiles en un parche.
+    */
     protected double calcularProbDetEst(int C, int A) {
         double probabilidad=1-Math.exp(-K*Math.round(C/A));
         return probabilidad;
     }
 
+    /**
+    * Metodo calculador del riesgo neto que tiene un agente al nacer.
+    * @return Riesgo del agente a ser encarcelado si se subleva.
+    */
     protected double calcularRiesgo() {
         double riesgoNeto=this.aversionR*this.probDetEst;
         return riesgoNeto;
     }
 
+    /**
+    * Metodo verificador del estado de un agente, si es activo o pasivo.
+    * @return 1 si esta rebelado o 0 si es pasivo o inactivo.
+    */
     public void verificarEstado() {
         if (this.agravio-this.riesgoN>LIMITE){
             this.estado=1;
@@ -78,6 +90,9 @@ public class Agente extends Persona {
         }
     }
     
+    /**
+    * Metodos getter y setter del constructor de la clase Agente.
+    */
     public double getPerjuicioPercibido(){
         return perjuicioPercibido;
     }
@@ -142,8 +157,13 @@ public class Agente extends Persona {
         this.posAgente=posAgente;
     }
 
+    /**
+    * Metodo sobreescrito ToString para en caso de imprimir un objeto de la clase.
+    * @return Cadena de texto con los atributos del objeto de la clase.
+    */
     @Override
     public String toString() {
         return "Agente{" + "perjuicioPercibido=" + perjuicioPercibido + ", agravio=" + agravio + ", aversionR=" + aversionR + ", probDetEst=" + probDetEst + ", riesgoN=" + riesgoN + '}';
     }
 }
+//Fin de la clase.
